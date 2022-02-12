5c50e4c2ecd403eb954acf65a3aae79b3ca3ff86
Laura THAY <laura.thay@ipag.fr>
2022-02-12 18:32:54 +0100

    Added Nav and Search Area

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Nav = () => {
+    
+    return (
+        <nav>
+            <div className="nav-wrapper container">
+                <a href="#" className="brand-logo"> Movie finder </a>
+            </div>
+        </nav>
+    )
+}
+
